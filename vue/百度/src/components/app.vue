a059f8dc507e40bcfb683f75d1f1b5254ec1d1b6
fengxingxue <531149553@qq.com>
2017-11-30 11:05:18 +0800

    vue

---
+++ vue/百度/src/components/app.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <hed></hed>
+        <div class="main">
+            <fish></fish>
+            <mytext></mytext>
+        </div>
+        <foot></foot>
+    </div>
+</template>
+
+
+<script>
+    import hed from './hed.vue'
+    import fish from './fish.vue'
+    import mytext from './mytext.vue'
+    import foot from './foot.vue'
+export default{
+            components:{
+                hed,
+                fish,
+                mytext,
+                foot
+            }
+}
+
+</script>
+
+
+<style scoped>
+    .main{
+        padding-left:20px;
+        padding-right:20px;
+        background: #fff;
+    }
+</style>
