12c24241b7c3c8fdbb68faeb916f0dd713542d61
fengxingxue <531149553@qq.com>
2017-11-30 10:56:06 +0800

    jingdong
    京东购物车

---
+++ 京东/src/components/app.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <myheader></myheader>
+        <mymain></mymain>
+    </div>
+</template>
+
+<script>
+    import myheader from './myheader.vue'
+    import mymain from './mymain.vue'
+    export default{
+        components:{
+            myheader,
+            mymain
+        }
+    }
+</script>
+
+<style>
+ *{
+     padding:0;
+     margin:0;
+     box-sizing: border-box;
+ }
+</style>
