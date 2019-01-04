3d961baae9ecf7df64d6733ed8470b7d32ecf6ce
fengxingxue <531149553@qq.com>
2019-01-04 16:57:58 +0800

    Add files via upload

--- haiwang/js/index.js
+++ haiwang/js/index.js
@@ -1,42 +1,34 @@
 var width = document.documentElement.clientWidth;
 var height = document.documentElement.clientHeight;
 var screen_width = width; //屏幕宽度
-if (width < height) {
+window.onload=function(){
+    if (width < height) {
         screen_width = height; //如果 是竖屏，灵感的宽度就等于屏高
         var contentDOM = document.getElementById('all');
+        var contentDOM1 = document.getElementById('all1');
+        var contentDOM2 = document.getElementById('all2');
         contentDOM.style.width = height + 'px';
         contentDOM.style.height = width + 'px';
         contentDOM.style.top = (height - width) / 2 + 'px';
         contentDOM.style.left = 0 - (height - width) / 2 + 'px';
         contentDOM.style.transform = 'rotate(90deg)';
-    }
-window.onload=function(){
-    
 
-    var story = document.getElementById('word');
-    var s = document.getElementById('show');
-    var i = 0;
-    timer = setInterval(function () {
-        s.innerHTML = story.innerHTML.substring(0, i);
-        i++;
-        if (s.innerHTML == story.innerHTML) {
-            clearInterval(timer);
-             s.style.display='none'
-            var story1 = document.getElementById('word1');
-            var s1 = document.getElementById('show1');
-            var ii = 0;
-            timer1 = setInterval(function () {
-                s1.innerHTML = story1.innerHTML.substring(0, ii);
-                ii++;
-                if (s1.innerHTML == story1.innerHTML) {
-                    clearInterval(timer1);
-                }
-            }, 200);
-        }
-    }, 200);
+
+        contentDOM1.style.width = height + 'px';
+        contentDOM1.style.height = width + 'px';
+        contentDOM1.style.top = (height - width) / 2 + 'px';
+        contentDOM1.style.left = 0 - (height - width) / 2 + 'px';
+
+
+        contentDOM2.style.width = height + 'px';
+        contentDOM2.style.height = width + 'px';
+        contentDOM2.style.top = (height - width) / 2 + 'px';
+        contentDOM2.style.left = 0 - (height - width) / 2 + 'px';
+    }
 
 
 }
+
 
 function orientationHandler(event) {
     var num=event.gamma.toFixed(6)
@@ -45,6 +37,8 @@ function orientationHandler(event) {
         var absVal = Math.abs(num);
         screen_width = height;
         var contentDOM = document.getElementById('all');
+        var contentDOM1 = document.getElementById('all1');
+        var contentDOM2 = document.getElementById('all2');
         if(num==absVal && num>10){
             contentDOM.style.width = height + 'px';
             contentDOM.style.height = width + 'px';
@@ -52,6 +46,18 @@ function orientationHandler(event) {
             contentDOM.style.left = 0 - (height - width) / 2 + 'px';
             contentDOM.style.transform = 'rotate(-90deg)';
             contentDOM.style.transition='.5s'
+
+            contentDOM1.style.width = height + 'px';
+            contentDOM1.style.height = width + 'px';
+            contentDOM1.style.top = (height - width) / 2 + 'px';
+            contentDOM1.style.left = 0 - (height - width) / 2 + 'px';
+
+
+            contentDOM2.style.width = height + 'px';
+            contentDOM2.style.height = width + 'px';
+            contentDOM2.style.top = (height - width) / 2 + 'px';
+            contentDOM2.style.left = 0 - (height - width) / 2 + 'px';
+
         }else if(num!=absVal && num<-10){
             contentDOM.style.width = height + 'px';
             contentDOM.style.height = width + 'px';
@@ -59,6 +65,18 @@ function orientationHandler(event) {
             contentDOM.style.left = 0 - (height - width) / 2 + 'px';
             contentDOM.style.transform = 'rotate(90deg)';
             contentDOM.style.transition='.5s'
+
+            contentDOM1.style.width = height + 'px';
+            contentDOM1.style.height = width + 'px';
+            contentDOM1.style.top = (height - width) / 2 + 'px';
+            contentDOM1.style.left = 0 - (height - width) / 2 + 'px';
+
+
+            contentDOM2.style.width = height + 'px';
+            contentDOM2.style.height = width + 'px';
+            contentDOM2.style.top = (height - width) / 2 + 'px';
+            contentDOM2.style.left = 0 - (height - width) / 2 + 'px';
+
         }
     }
 }
