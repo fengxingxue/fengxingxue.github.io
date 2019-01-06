e4d70822aae3f5cd63f94b9509f199e8f7c8c639
fengxingxue <531149553@qq.com>
2019-01-06 10:24:45 +0800

    Update index.js

--- haiwang/js/index.js
+++ haiwang/js/index.js
@@ -10,21 +10,21 @@ var screen_width = width; //屏幕宽度
             screen_width = height; //如果 是竖屏，灵感的宽度就等于屏高
             contentDOM.style.width = height + 'px';
             contentDOM.style.height = width + 'px';
-            contentDOM.style.top ='-30px';
-            contentDOM.style.left = '0px';
+            contentDOM.style.top = (height - width) / 2 + 'px';
+            contentDOM.style.left = 0 - (height - width) / 2 + 'px';
           //  contentDOM.style.transform = 'rotate(90deg)';
 
 
             contentDOM1.style.width = height + 'px';
             contentDOM1.style.height = width + 'px';
-             contentDOM1.style.top ='0px';
-            contentDOM1.style.left = '0px';
+            contentDOM1.style.top = (height - width) / 2 + 'px';
+            contentDOM1.style.left = 0 - (height - width) / 2 + 'px';
 
 
             contentDOM2.style.width = height + 'px';
             contentDOM2.style.height = width + 'px';
-            contentDOM2.style.top ='0px';
-            contentDOM2.style.left = '0px';
+            contentDOM2.style.top = (height - width) / 2 + 'px';
+            contentDOM2.style.left = 0 - (height - width) / 2 + 'px';
         }
  
 
