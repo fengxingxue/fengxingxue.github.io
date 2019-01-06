87d24cdfb51f3e2b1eccd22772107caba8e05478
fengxingxue <531149553@qq.com>
2019-01-06 10:18:08 +0800

    Update index.js

--- haiwang/js/index.js
+++ haiwang/js/index.js
@@ -9,8 +9,8 @@ var screen_width = width; //屏幕宽度
         if (width < height) {
             screen_width = height; //如果 是竖屏，灵感的宽度就等于屏高
             contentDOM.style.width = height + 'px';
-            contentDOM.style.height = Number(width-20) + 'px';
-            contentDOM.style.top ='0px';
+            contentDOM.style.height = width + 'px';
+            contentDOM.style.top ='-30px';
             contentDOM.style.left = '0px';
           //  contentDOM.style.transform = 'rotate(90deg)';
 
